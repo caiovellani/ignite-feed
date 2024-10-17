08a575d16ae18a8ad7a4fa703d780486ed6c99ec
Caio Vellani <caiovellani@outlook.com>
2024-10-16 23:13:15 -0300

    feat: create Header

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import styles from './Header.module.css';
+
+import igniteLogo from '../assets/ignite-logo.svg';
+
+console.log(igniteLogo);
+
+export function Header() {
+    return (
+        <div>
+            <header className={styles.header}>
+                <img src={igniteLogo} alt="Ignite Logo"></img>
+            </header>            
+        </div>
+    )
+}
