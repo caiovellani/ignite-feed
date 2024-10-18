6416c8a7d995f9f2685b403111cf5737330cf281
Caio Vellani <caiovellani@outlook.com>
2024-10-17 22:30:41 -0300

    refactor: add Avatar component, and removed css avatar style

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import { PencilLine } from 'phosphor-react';
+import { Avatar } from './Avatar';
 
 import styles from './Sidebar.module.css';
 export function Sidebar() {
@@ -10,10 +11,7 @@ export function Sidebar() {
             />
 
             <div className={styles.profile}>
-                <img 
-                    className={styles.avatar}
-                    src="https://github.com/caiovellani.png" 
-                />
+                <Avatar src="https://github.com/caiovellani.png" />
                 <strong>Caio Vellani</strong>
                 <span>Web Developer</span>
             </div>
