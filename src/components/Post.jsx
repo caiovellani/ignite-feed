94f0c46e7bd97ddd5e1c65847a701c91a36f4819
Caio Vellani <caiovellani@outlook.com>
2024-10-17 22:29:54 -0300

    feat: create Post component, and Post style

---
+++ src/components/Post.jsx
@@ -0,0 +1,49 @@
+import { Avatar } from './Avatar';
+import { Comment } from './Comment';
+import styles from './Post.module.css';
+
+export function Post() {
+    return (
+        <article className={styles.post}>
+            <header>
+                <div className={styles.author}>
+                    <Avatar src="https://github.com/caiovellani.png" />
+                    <div className={styles.authorInfo}>
+                        <strong>Caio Vellani</strong>
+                        <span>Web Developer</span>
+                    </div>          
+                </div>
+
+                <time title="17 de outubro às 20:10" dateTime="2024-17-10 20:10:00">Publicado há 1h</time>
+            </header>
+
+            <div className={styles.content}>
+                <p>Fala galeraa 👋</p>
+                <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+                <p><a href="">jane.design/doctorcare</a></p>
+                <p>
+                    <a href="">#novoprojeto</a>{' '}
+                    <a href="">#nlw</a>{' '}
+                    <a href="">#rocketseat</a>
+                </p>
+            </div>
+
+            <form className={styles.commentForm}>
+                <strong>Deixe seu feedback</strong>
+                <textarea 
+                    placeholder="Deixe um comentário"
+                />
+
+                <footer>
+                    <button type="submit">Publicar</button>
+                </footer>
+            </form>
+
+            <div className={styles.commentList}>
+                <Comment />
+                <Comment />
+                <Comment />
+            </div>
+        </article>
+    );
+}
