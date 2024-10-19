e0f76495bf2ca86c4d06daa05b7cdc24931f63f8
Caio Vellani <caiovellani@outlook.com>
2024-10-19 19:12:26 -0300

    refactor: implement function for invalidComments or no comments for publish, and disable button if invalidComment is true, update css button to disabled hover and disabled only

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -30,7 +30,12 @@ export function Post({ author, publishedAt, content }) {
     }
 
     function handleNewCommentChange() {
+        event.target.setCustomValidity('');
         setNewCommentText(event.target.value)
+    }
+
+    function handleNewCommentInvalid() {
+        event.target.setCustomValidity('Este campo é obrigatório');
     }
 
     function deleteComment(commentToDelete) {
@@ -40,6 +45,8 @@ export function Post({ author, publishedAt, content }) {
 
         setComments(commentsWithoutDeletedOne);
     }
+
+    const isNewCommentEmpty = newCommentText.length === 0;
 
     return (
         <article className={styles.post}>
@@ -76,10 +83,14 @@ export function Post({ author, publishedAt, content }) {
                     placeholder="Deixe um comentário"
                     onChange={handleNewCommentChange}
                     value={newCommentText}
+                    onInvalid={handleNewCommentInvalid}
+                    required
                 />
 
                 <footer>
-                    <button type="submit">Publicar</button>
+                    <button type="submit" disabled={isNewCommentEmpty}>
+                        Publicar
+                    </button>
                 </footer>
             </form>
 
